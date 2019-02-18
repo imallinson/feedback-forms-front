0f309cf9439aad338f18060805b13e1af43d8363
JoelHalford <jason.derry@academytrainee.com>
2019-02-18 18:09:22 +0000

    setup layout and styling for frontpage and navbar, including google fonts

--- src/components/HomepageComponent.js
+++ src/components/HomepageComponent.js
@@ -7,7 +7,20 @@ class HomepageComponent extends Component {
   render() {
     return (
     	<div class="main-body">
-			<h1>Hello from homepage.</h1>
+			<h1 id="homepage-welcome">Hello, Matt.</h1>
+			<div class="home-body">
+				<div id="dashboard-item-1">
+				COHORTS (9)
+				</div>
+				<div>
+					<div id="dashboard-item-2">
+						TRAINEES (5)
+					</div>
+					<div id="dashboard-item-3">
+						ACCOUNT
+					</div>
+				</div>
+			</div>
 		</div>
     );
   }
