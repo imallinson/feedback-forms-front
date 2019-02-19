8235d53db304bce54790dc8d8f6a85215736ba18
imallinson <imallinson1001@gmail.com>
2019-02-19 16:47:17 +0000

    removed unused import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/NavbarComponent';
-import dragula from 'dragula';
 
 class App extends Component {
   render() {
