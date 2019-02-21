de1cea5d00ab417ed7a0f22e9f746fac436080fe
JoelHalford <topsupp@hotmail.com>
2019-02-21 12:29:43 +0000

    fixed routes

--- src/Routes.js
+++ src/Routes.js
@@ -6,11 +6,15 @@ import {
     Link
 } from 'react-router-dom';
 import Homepage from './components/HomepageComponent';
+import SubmitForm from './components/FormComponent';
 import Cohorts from './components/CohortsComponent';
 import Trainees from './components/TraineesComponent';
 import Account from './components/AccountComponent';
 import Register from './components/RegisterComponent';
 import Login from './components/LoginComponent';
+import Cohort from './components/SingleCohortComponent';
+import User from './components/SingleUserComponent';
+import ViewForm from './components/ViewFormComponent';
 
 class Routes extends Component {
   render() {
@@ -18,11 +22,15 @@ class Routes extends Component {
   	<div>
 		<Route exact path="/" component={ Homepage } />
 		<Route exact path="/home" component={ Homepage } />
+		<Route exact path="/form" component={ SubmitForm } />
 		<Route exact path="/cohorts" component={ Cohorts } />
 		<Route exact path="/trainees" component={ Trainees } />
 		<Route exact path="/account" component={ Account } />
 		<Route exact path="/register" component={ Register } />
 		<Route exact path="/login" component={ Login } />
+		<Route exact path="/singlecohort" component={ Cohort } />
+		<Route exact path="/singleuser" component={ User } />
+		<Route exact path="/viewform" component={ ViewForm } />
 	</div>
 	)}
 }
