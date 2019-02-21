d1bf06ac64ab56c2d32c8f06ea8bac4e3928caa1
JoelHalford <topsupp@hotmail.com>
2019-02-21 14:31:47 +0000

    homepage has 3 dynamic displays for not logged in, logged in as trainer and logged in as trainee

--- src/components/HomepageComponent.js
+++ src/components/HomepageComponent.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
 import '../App.css';
+import auth from '../Auth';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
+
 class HomepageComponent extends Component {
 
   render() {
     return (
     	<div className="main-body">
-			<h1 id="heading">Hello, Matt.</h1>
-			<div className="home-body">
-				<div id="dashboard-item-1">
-					<a href="/cohorts">COHORTS (9)</a>
-				</div>
-				<div id="right-dashboard">
-					<div id="dashboard-item-2">
-						<a href="/trainees">TRAINEES (5)</a>
+				{JSON.parse(auth.isAuthenticated()) ?
+					<div className="home-body">
+						<div id="dashboard-item-1">
+							{JSON.parse(auth.isAuthenticated() && cookies.get('admin') == 'true') ? <a href="/cohorts">COHORTS (9)</a> : <a href="/form">FORM</a>}
+						</div>
+
+						<div id="right-dashboard">
+							<div id="dashboard-item-2">
+								{JSON.parse(auth.isAuthenticated() && cookies.get('admin') == 'true') ? <a href="/trainees">TRAINEES (5)</a> : <a href="/account">ACCOUNT</a>}
+							</div>
+							<div id="dashboard-item-3">
+								{JSON.parse(auth.isAuthenticated() && cookies.get('admin') == 'true') ? <a href="/account">ACCOUNT</a> : <a href="/home" onClick={() => {auth.logout(() => {});}}>LOGOUT</a>}
+							</div>
+						</div>
 					</div>
-					<div id="dashboard-item-3">
-						<a href="/account">ACCOUNT</a>
+
+					:
+
+					<div className="home-body">
+						<div id="dashboard-item-1">
+							<a href="/register">REGISTER</a>
+						</div>
+
+						<div id="right-dashboard">
+							<div id="dashboard-item-2-not-logged">
+								<a href="/login">LOGIN</a>
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
+
+				}
 		</div>
     );
   }
