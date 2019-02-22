7d620424aedd084d461951dad7e9f3901385079d
imallinson <45758809+imallinson@users.noreply.github.com>
2019-02-22 13:00:15 +0000

    docker network setup

--- src/Consts.js
+++ src/Consts.js
@@ -1,12 +1,7 @@
 const constants = {
-	newCohort: "http://localhost:8081",
-	newAccount: "http://localhost:8080",
-	get: "http://localhost:8086"
-
-	// newCohort: "/gateway/feedBackForms",
-	// newAccount: "/gateway/feedBackForms",
-	// get: "/gateway/feedBackForms"
-
+	newCohort: "/cohorts/",
+	newAccount: "/accounts/",
+	get: "/retriever/"
 }
 
 module.exports = constants
