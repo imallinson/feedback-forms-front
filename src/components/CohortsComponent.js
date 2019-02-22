11fa5491cf565e90bd0c65c189701333c518e1ed
imallinson <45758809+imallinson@users.noreply.github.com>
2019-02-22 13:49:47 +0000

    fix

--- src/components/CohortsComponent.js
+++ src/components/CohortsComponent.js
@@ -14,7 +14,7 @@ class CohortsComponent extends Component {
 
 		axios({
 			method: 'get',
-			url: constants.get + 'getCohorts'
+			url: constants.get + '/getCohorts'
 		}).then(response => {
 
 			this.setState({
